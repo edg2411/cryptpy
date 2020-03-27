d237d503dd51b5c8b79843fa4880a49cd7e2d672
edg2411 <55071106+edg2411@users.noreply.github.com>
2020-03-27 18:07:17 -0300

    Delete mqtt_usu.py

--- mqtt_usu.py
+++
@@ -1,9 +0,0 @@
-
-usu = {
-    "broker_address"    :   "farmer.cloudmqtt.com",  #Broker address
-    "port"              :   14766,                         #Broker port
-    "user"              :   "xdfykvhs",                    #Connection username
-    "password"          :   "QHxSy31yCzz-"            #Connection password    
-}
-
-# print(usu)
