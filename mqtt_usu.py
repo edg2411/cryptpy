a200d56167aab3f8bd36ff4556fdddbe2bec871f
Ezequiel Di Giulio <eze.digiulio@gmail.com>
2020-03-27 17:50:54 -0300

    primer commit encriptado

---
+++ mqtt_usu.py
@@ -0,0 +1,9 @@
+
+usu = {
+    "broker_address"    :   "farmer.cloudmqtt.com",  #Broker address
+    "port"              :   14766,                         #Broker port
+    "user"              :   "xdfykvhs",                    #Connection username
+    "password"          :   "QHxSy31yCzz-"            #Connection password    
+}
+
+# print(usu)
